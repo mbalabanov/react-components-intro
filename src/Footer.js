f1d2685cf9c8b0ebca4402080338d285c5d02c90
mbalabanov <marincomics@gmail.com>
2022-09-20 14:01:37 +0200

    Splits App.js into separate components

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer>
+      <p className="country">
+        United Kingdom of Great Britain and Northern Ireland
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,18 @@
+function Main() {
+  return (
+    <main>
+      <img
+        className="logo"
+        src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+        alt="Google logo"
+      />
+      <input className="search-bar" type="search" />
+      <div className="buttons">
+        <button>Google Search</button>
+        <button>Feeling Funky</button>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
